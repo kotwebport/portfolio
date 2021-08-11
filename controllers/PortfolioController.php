e73c6aca1b41cac3b89d5a0d2b1f3ccc17ac9d8e
kotwebport <kotwebport@gmail.com>
2021-08-11 16:16:17 +0300

    решил вновь заняться портфолио

--- controllers/PortfolioController.php
+++ controllers/PortfolioController.php
@@ -5,8 +5,24 @@ namespace app\controllers;
 class PortfolioController extends AppController
 {
 
-    public function actionIndex()
+	public function actionIndex()
     {
         return $this->render('index');
     }
+	
+	/**
+		Лендинг
+	*/
+		
+	
+	public function actionLandingPage($page)
+	{
+				
+		return $this->render('landing', [
+			'page' => $page,
+		]);
+		
+	}
+	
+	
 }
---
+++ views/portfolio/landing.php
@@ -0,0 +1,5 @@
+<?php $page="gklwekrekerkkern"?>
+<?php	if($page): ?>
+
+<h1 style="color: orange; weght: 900; " ><?= $page ?>  </h1>
+<?php endif; ?>	
