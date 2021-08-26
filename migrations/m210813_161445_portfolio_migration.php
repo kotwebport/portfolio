38d64d4e8cd117746cf1487b3afce193821e633f
kotwebport <kotwebport@gmail.com>
2021-08-26 21:27:14 +0300

    автонавигация

--- migrations/m210813_161445_portfolio_migration.php
+++ migrations/m210813_161445_portfolio_migration.php
@@ -18,13 +18,16 @@ class m210813_161445_portfolio_migration extends Migration
         $this->insert('navigation_work', ['direction_id' => '1', 'link' => 'landing', 'name' => 'Лендинг-3', 'image' => 'landing3.png']);
         $this->insert('navigation_work', ['direction_id' => '1', 'link' => 'landing', 'name' => 'Лендинг-4', 'image' => 'landing4.png']);
         $this->insert('navigation_work', ['direction_id' => '1', 'link' => 'landing', 'name' => 'Лендинг-5', 'image' => 'landing5.png']);
+        $this->insert('navigation_work', ['direction_id' => '1', 'link' => 'landing', 'name' => 'Лендинг-6', 'image' => 'landing6.png']);
         $this->insert('navigation_work', ['direction_id' => '2', 'link' => 'visiting', 'name' => 'Визитка-1', 'image' => 'visiting1.png']);
         $this->insert('navigation_work', ['direction_id' => '2', 'link' => 'visiting', 'name' => 'Визитка-2', 'image' => 'visiting2.png']);
         $this->insert('navigation_work', ['direction_id' => '2', 'link' => 'visiting', 'name' => 'Визитка-3', 'image' => 'visiting3.png']);
         $this->insert('navigation_work', ['direction_id' => '2', 'link' => 'visiting', 'name' => 'Визитка-4', 'image' => 'visiting4.png']);
+        $this->insert('navigation_work', ['direction_id' => '2', 'link' => 'visiting', 'name' => 'Визитка-5', 'image' => 'visiting5.png']);
         $this->insert('navigation_work', ['direction_id' => '3', 'link' => 'info', 'name' => 'Инфо-1', 'image' => 'info1.png']);
         $this->insert('navigation_work', ['direction_id' => '3', 'link' => 'info', 'name' => 'Инфо-2', 'image' => 'info2.png']);
         $this->insert('navigation_work', ['direction_id' => '3', 'link' => 'info', 'name' => 'Инфо-3', 'image' => 'info3.png']);
+        $this->insert('navigation_work', ['direction_id' => '3', 'link' => 'info', 'name' => 'Инфо-4', 'image' => 'info4.png']);
         $this->insert('navigation_work', ['direction_id' => '4', 'link' => 'personal', 'name' => 'Персональный-1', 'image' => 'personal1.png', 'best' => true]);
         $this->insert('navigation_work', ['direction_id' => '4', 'link' => 'personal', 'name' => 'Персональный-2', 'image' => 'personal2.png']);
         $this->insert('navigation_work', ['direction_id' => '4', 'link' => 'personal', 'name' => 'Персональный-3', 'image' => 'personal3.png']);
@@ -42,7 +45,6 @@ class m210813_161445_portfolio_migration extends Migration
         $this->insert('navigation_work', ['direction_id' => '8', 'link' => 'social', 'name' => 'Соц.сеть-3', 'image' => 'social3.png']);
         $this->insert('navigation_work', ['direction_id' => '9', 'link' => 'multimedia', 'name' => 'Мультимедиа-1', 'image' => 'multimedia1.png']);
         $this->insert('navigation_work', ['direction_id' => '9', 'link' => 'multimedia', 'name' => 'Мультимедиа-2', 'image' => 'multimedia2.png']);
-        $this->insert('navigation_work', ['direction_id' => '9', 'link' => 'multimedia', 'name' => 'Мультимедиа-3', 'image' => 'multimedia3.png']);
     }
 
 
--- web/js/portfolio.js
+++ web/js/portfolio.js
@@ -3,15 +3,29 @@ $('document').ready(function () {
     /**
      * Слайдер навигации
      */
+    var bestWorks = $('div#work-nav-best a.work');
+    setMargin(bestWorks);
+    $('div#work-nav-best').css('display', 'inline-block');
+
 
     $('div.direction').on('click', function () {
         $('div#work-nav-best').css('display', 'none');
         $('div.work-nav-open').removeClass('work-nav-open');
         var directionId = $(this).data('id');
-        var workNav = $('div.work-nav[data-id=' + directionId + ']');
-        workNav.addClass('work-nav-open');
+        $('div.work-nav[data-id=' + directionId + ']').addClass('work-nav-open');
+        var openWorks = $('div.work-nav-open a.work');
+        setMargin(openWorks);
+
     });
-
-
 });
+
+/**
+ * Функция автоматической установки полей для представленных работ
+ * @param works
+ */
+function setMargin(works) {
+    var margin = -((30 * works.length - 70) / works.length * 1.09) / 2 + '%';
+    works.css({'margin-left': margin, 'margin-right': margin});
+}
+
 
